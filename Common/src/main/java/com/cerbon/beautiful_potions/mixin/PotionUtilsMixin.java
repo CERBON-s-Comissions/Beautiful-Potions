27fcd14359c16238343d0bab2a73e479ffc6c488
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-07-29 18:06:04 -0300

    Fix arrows not having colors

--- Common/src/main/java/com/cerbon/beautiful_potions/mixin/PotionUtilsMixin.java
+++ Common/src/main/java/com/cerbon/beautiful_potions/mixin/PotionUtilsMixin.java
@@ -1,5 +1,7 @@
 package com.cerbon.beautiful_potions.mixin;
 
+import net.minecraft.world.item.ArrowItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.PotionUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -9,8 +11,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(PotionUtils.class)
 public abstract class PotionUtilsMixin {
 
-    @Inject(method = "getColor(Ljava/util/Collection;)I", at = @At("HEAD"), cancellable = true)
-    private static void getColor(CallbackInfoReturnable<Integer> cir) {
-        cir.setReturnValue(0xFFFFFF);
+    @Inject(method = "getColor(Lnet/minecraft/world/item/ItemStack;)I", at = @At("HEAD"), cancellable = true)
+    private static void getColor(ItemStack stack, CallbackInfoReturnable<Integer> cir) {
+        if (!(stack.getItem() instanceof ArrowItem))
+            cir.setReturnValue(0xFFFFFF);
     }
 }
