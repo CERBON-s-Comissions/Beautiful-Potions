9712a67cee70b9dffd6aa44e15ff2875ddadae39
felip <felipecerboncinix@gmail.com>
2025-08-24 16:16:07 -0300

    Fix removing colors from other mods potions

--- Common/src/main/java/com/cerbon/beautiful_potions/mixin/PotionUtilsMixin.java
+++ Common/src/main/java/com/cerbon/beautiful_potions/mixin/PotionUtilsMixin.java
@@ -1,5 +1,6 @@
 package com.cerbon.beautiful_potions.mixin;
 
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.PotionUtils;
@@ -13,7 +14,7 @@ public abstract class PotionUtilsMixin {
 
     @Inject(method = "getColor(Lnet/minecraft/world/item/ItemStack;)I", at = @At("HEAD"), cancellable = true)
     private static void getColor(ItemStack stack, CallbackInfoReturnable<Integer> cir) {
-        if (!(stack.getItem() instanceof ArrowItem))
+        if (!(stack.getItem() instanceof ArrowItem) && BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace().equals("minecraft"))
             cir.setReturnValue(0xFFFFFF);
     }
 }
