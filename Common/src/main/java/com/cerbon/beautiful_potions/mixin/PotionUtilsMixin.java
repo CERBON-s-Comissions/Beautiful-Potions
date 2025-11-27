f22cd1146191ac27c84a31508ff40e4e8a1305c0
Felipe Cerboncini Cordeiro <felipecerboncinix@gmail.com>
2025-11-26 23:25:16 -0300

    Prevent removing colors from other mods

--- Common/src/main/java/com/cerbon/beautiful_potions/mixin/PotionUtilsMixin.java
+++ Common/src/main/java/com/cerbon/beautiful_potions/mixin/PotionUtilsMixin.java
@@ -1,8 +1,11 @@
 package com.cerbon.beautiful_potions.mixin;
 
+import com.cerbon.beautiful_potions.BeautifulPotions;
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.alchemy.Potion;
 import net.minecraft.world.item.alchemy.PotionUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,7 +17,15 @@ public abstract class PotionUtilsMixin {
 
     @Inject(method = "getColor(Lnet/minecraft/world/item/ItemStack;)I", at = @At("HEAD"), cancellable = true)
     private static void getColor(ItemStack stack, CallbackInfoReturnable<Integer> cir) {
-        if (!(stack.getItem() instanceof ArrowItem) && BuiltInRegistries.ITEM.getKey(stack.getItem()).getNamespace().equals("minecraft"))
-            cir.setReturnValue(0xFFFFFF);
+        if (!(stack.getItem() instanceof ArrowItem)) {
+            Potion potion = PotionUtils.getPotion(stack);
+            ResourceLocation potionRL = BuiltInRegistries.POTION.getKey(potion);
+            if (potionRL == null) return;
+
+            String potionNamespace = potionRL.getNamespace();
+
+            if (BeautifulPotions.POTION_NAMESPACES.contains(potionNamespace))
+                cir.setReturnValue(0xFFFFFF);
+        }
     }
 }
